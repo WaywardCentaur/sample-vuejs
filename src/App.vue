9fad09c7aece0512e5af5e7a93187a117e390fa4
Raymond Mauge <raymondandre95@gmail.com>
2023-11-28 11:42:17 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
 <template>
   <div id="app" class="bg-green-600 py-56 min-h-screen">
-    <div class="container mx-auto text-center px-8">
-      <h2 class="text-3xl lg:text-6xl font-bold mb-2 text-white">Welcome to Your Vue App</h2>
-      <h3 class="text-xl lg:text-4xl text-green-200">
-        Deployed to
+    <div class="container mx-auto text-center px-8"> 
+      <h2 class="text-3xl lg:text-6xl font-bold mb-2 text-green-200">
+        Presenting
         <span
           class="font-bold bg-clip-text text-transparent bg-gradient-to-br from-blue-400 via-purple-400 to-blue-500"
-        >DigitalOcean</span>
-      </h3>
+        >Us the Others</span>
+      </h2>
+      <h3 class="text-3xl lg:text-6xl font-bold mb-2 text-white">Description</h3>
 
       <div class="flex justify-center space-x-2 mt-6 lg:mt-10">
-        <a
-          href="https://www.digitalocean.com/docs/app-platform"
-          class="inline-block py-2 lg:py-4 px-4 lg:px-8 rounded bg-green-500 hover:bg-green-400 text-green-100 shadow hover:shadow-2xl transition duration-300"
-        >View the Docs</a>
-        <a
+        <img :src="image" alt="Concept art 1" />
+        <v-btn
+        color="primary"
+        @click="dialog = true"
+        >
+          Open Dialog
+        </v-btn>
+
+        <v-dialog
+          v-model="dialog"
+          width="auto"
+        >
+          <v-card>
+            <v-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <!-- <a
           href="https://cloud.digitalocean.com/apps"
           class="inline-block py-2 lg:py-4 px-4 lg:px-8 rounded bg-yellow-400 hover:bg-yellow-300 text-yellow-800 shadow hover:shadow-2xl transition duration-300"
-        >View Your Dashboard</a>
+        >View Your Dashboard</a> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Image from '@/assets/image.png'; 
 export default {
   name: "App",
+  data() {
+    return {
+      image: Image,
+      dialog: false,
+    };
+  },
 };
 </script>
 
